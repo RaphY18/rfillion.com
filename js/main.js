ffc3fa098ab792b79d7141c3d0bd2131258406a7
RaphY18 <34820473+RaphY18@users.noreply.github.com>
2020-08-31 18:19:49 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-//Just to not be empty
+// just to put some content
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav__links");
+  const navlinks = document.querySelectorAll(".nav__links li");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navlinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = " ";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease-in-out forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
+    });
+
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
